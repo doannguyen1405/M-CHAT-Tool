c69a48deb0528ef5230e534a4306119bfbb4f58b
doannguyen1405 <141898405+doannguyen1405@users.noreply.github.com>
2026-01-21 10:35:49 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -100,7 +100,7 @@ const App: React.FC = () => {
       `;
 
       const response = await ai.models.generateContent({
-        model: 'gemini-1.5-flash',
+        model: 'gemini-3-flash-preview',
         contents: prompt,
         generationConfig: {
       maxOutputTokens: 512,
